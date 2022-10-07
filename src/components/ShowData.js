a704c61261e3e3052660df297b0fccd9b730b2fc
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2022-10-07 16:33:12 +0200

    created the design

--- src/components/ShowData.js
+++ src/components/ShowData.js
@@ -1,13 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-class ShowData extends Component{
-    render(){
-        return(
-            <div>
-                ShowData
-            </div>
-        )
-    }
+class ShowData extends Component {
+  render() {
+    return (
+      <div className="data">
+        <div className="list">
+          <div className="item">
+            country : <span />
+          </div>
+          <div className="item">
+            last_updated : <span />
+          </div>
+          <div className="item">
+            temp_c : <span />
+          </div>
+          <div className="item">
+            humidity : <span />
+          </div>
+          <div className="item">
+            weather state : <span />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default ShowData
+export default ShowData;

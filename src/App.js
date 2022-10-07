330276b7eb167d3bd6961585e62255abe80f2a6d
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2022-10-07 21:02:04 +0200

    deploy on github

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ShowData from "./components/ShowData";
 import { Component } from "react";
 import axios from "axios";
 const API_KEY = "3e5ffc95afa94dd59f1213920220610";
-const WEB_LINK = "http://api.weatherapi.com/v1/current.json";
+const WEB_LINK = "https://api.weatherapi.com/v1/current.json";
 
 class App extends Component {
   state = {
@@ -87,34 +87,36 @@ class App extends Component {
     /*=============== 2 ============== */
 
     /*=============== 3 ============== */
-    axios
-      .get(`${WEB_LINK}?key=${API_KEY}&q=${e.target.country.value}&aqi=no`)
-      .then(res => {
-        let data = res.data;
-        this.setState({
-          country: data.location.country,
-          city: data.location.name,
-          region: data.location.region,
-          last_updated: data.current.last_updated,
-          temp_c: data.current.temp_c,
-          humidity: data.current.humidity,
-          text: data.current.condition.text,
-          error: ""
+    if (e.target.country.value) {
+      axios
+        .get(`${WEB_LINK}?key=${API_KEY}&q=${e.target.country.value}&aqi=no`)
+        .then(res => {
+          let data = res.data;
+          this.setState({
+            country: data.location.country,
+            city: data.location.name,
+            region: data.location.region,
+            last_updated: data.current.last_updated,
+            temp_c: data.current.temp_c,
+            humidity: data.current.humidity,
+            text: data.current.condition.text,
+            error: ""
+          });
+        })
+        .catch(error => {
+          console.log(error);
+          this.setState({
+            country: "",
+            city: "",
+            region: "",
+            last_updated: 0,
+            temp_c: 0,
+            humidity: "",
+            text: "",
+            error: error.response.data.error.message
+          });
         });
-      })
-      .catch(error => {
-        console.log(error);
-        this.setState({
-          country: "",
-          city: "",
-          region: "",
-          last_updated: 0,
-          temp_c: 0,
-          humidity: "",
-          text: "",
-          error: error.response.data.error.message
-        });
-      });
+    }
     /*=============== 3 ============== */
   }
 
